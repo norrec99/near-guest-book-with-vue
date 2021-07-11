cb9df7aab7d687cc67449b9630ac8f7e30e1b4fd
cagatay soyer <cagataysoyer@gmail.com>
2021-07-11 15:53:54 +0300

    form added

--- src/components/BaseForm.vue
+++ src/components/BaseForm.vue
@@ -9,21 +9,27 @@
               <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Welcome {{ accountId }}</h2>
               <p class="mt-4 text-lg text-gray-500">Sign in with NEAR and add a message to the guest book!</p>
               <div class="mt-6">
-                <label class="block text-sm font-medium text-gray-700">Message</label>
-                <div class="mt-1">
-                  <input type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Message" />
-                </div>
-                <label class="block text-sm font-medium text-gray-700">Message</label>
-                <div class="mt-1">
-                  <input type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Message" />
-                </div>
+                <form>
+                  <label class="block text-sm font-medium text-gray-700">Message</label>
+                  <div class="mt-1">
+                    <input v-model="message" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Message" />
+                  </div>
+                  <label class="mt-2 block text-sm font-medium text-gray-700">Donation</label>
+                  <div class="mt-1">
+                    <input v-model="donation" type="number" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="0" />
+                  </div>
+                  <button @click="onSubmit" type="button" class="mt-4 inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Send message
+                    <MailIcon class="ml-2 -mr-0.5 h-4 w-4" aria-hidden="true" />
+                  </button>
+                </form>
               </div>
             </div>
           </div>
         </div>
         <div class="mt-12 sm:mt-16 lg:mt-0">
           <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://near-guestbook.onrender.com/img/hero.57ea1a39.png" alt="Inbox user interface" />
+            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://www.edmmtnbike.ca/wp-content/uploads/2019/03/email.png" alt="Inbox user interface" />
           </div>
         </div>
       </div>
@@ -33,14 +39,25 @@
 <script>
 import { ref } from "vue";
 
+import { MailIcon } from "@heroicons/vue/solid";
+
 import { wallet } from "@/services/near";
 
 export default {
+  props: {
+    addMessage: {
+      type: Function,
+      required: true
+    }
+  },
+  components: {
+    MailIcon
+  },
   setup(props) {
     const message = ref("");
-    const donation = ref(null);
+    const donation = ref(0);
     const onSubmit = () => {
-      props.addMessages({ text: message.value, donation: donation.value });
+      props.addMessage({ text: message.value, donation: donation.value });
     };
     return {
       message,
@@ -48,12 +65,6 @@ export default {
       onSubmit,
       accountId: wallet.getAccountId()
     };
-  },
-  props: {
-    addMessages: {
-      type: Function,
-      required: true
-    }
   }
 };
 </script>
